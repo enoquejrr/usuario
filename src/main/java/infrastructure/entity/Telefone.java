25bd0356c24a3c7246799115b6caf19056c9146f
Enoque <enoquejrr@gmail.com>
2025-09-19 16:11:02 -0300

    adicionadas classes de security

---
+++ src/main/java/infrastructure/entity/Telefone.java
@@ -0,0 +1,26 @@
+package infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "telefones")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero", length = 15)
+    private String numero;
+
+    @Column(name = "ddd", length = 2)
+    private String ddd;
+}
---
+++ src/main/java/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package infrastructure.repository;
+
+
+import infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
